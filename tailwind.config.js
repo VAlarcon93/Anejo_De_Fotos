da1ef9b8e563504082ff3d95e93fcbcaaac6d19b
Vanessa Alarcón <57737540+VAlarcon93@users.noreply.github.com>
2023-06-27 21:07:38 -0600

    1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors:{
+      'Rojo':    '#E2231A',
+      'Azul':    '#263562',
+      'Negro':   '#383636',
+      'Celeste': '#D9DDE8',
+      'Gris':    '#F4EFEF',
+    }
+  },
+  plugins: [],
+}
+
