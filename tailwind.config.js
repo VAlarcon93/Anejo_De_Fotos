64e92a2b6b2ac324e8407454aea45860186227ec
Vanessa Alarcón <57737540+VAlarcon93@users.noreply.github.com>
2023-06-30 17:32:42 -0600

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Font1:['Work Sans', 'sans-serif'],
+        Font2 :['Arial Black', 'sans-serif'],
+        Font3 :['Century Gothic','sans-serif'],
+
+      },
+    },
     colors:{
       'Rojo':    '#E2231A',
       'Azul':    '#263562',
